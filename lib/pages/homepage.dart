87de530e29c7b41154bd0a2625cb4f2775b9d8da
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-03-07 15:58:44 +0200

    feat: dialog boxes for deleting and editing expenses

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  //* new expense bottom sheet
   void newExpense() {
     showModalBottomSheet(
       context: context,
@@ -88,6 +89,108 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //* edit expense dialog box
+  void openEditBox(Expense expense, BuildContext context) {
+    //* pre-load text fields with current values
+    String existingName = expense.name;
+    String existingAmount = expense.amount.toString();
+
+    //* show dialog to edit expense
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          }, 
+          child: const Text('Edit Expense'),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: existingName),
+              controller: nameController,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: existingAmount),
+              keyboardType: TextInputType.number,
+              controller: amountController,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    //* save as long as one field is filled
+                    
+                    if (
+                      nameController.text.isNotEmpty ||  amountController.text.isNotEmpty
+                    ) {
+                      //*pop box
+                      Navigator.pop(context);
+
+                      //*create new updated expense
+                      Expense updatedExpense = Expense(
+                        name: nameController.text.isNotEmpty ? nameController.text : existingName,
+                        amount: amountController.text.isNotEmpty ? double.parse(amountController.text) : double.parse(existingAmount),
+                        date: DateTime.now()
+                      );
+
+                      //* old expense id
+                      int existingId = expense.id;
+
+                      //*save to db
+                      await context
+                          .read<ExpenseDatabase>()
+                          .updateExpense(existingId, updatedExpense);
+
+                      //*clear text fields
+                      nameController.clear();
+                      amountController.clear();
+                    }
+                  },
+                  child: const Text('Save'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
+  //* delete expense dialob box
+  void openDeleteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Delete Expense'),
+        content: const Text('Are you sure you want to delete this expense?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Consumer<ExpenseDatabase>(
@@ -109,6 +212,8 @@ class _HomePageState extends State<HomePage> {
                 //* return ListTile
                 title: individualExpense.name,
                 trailing: formatAmount(individualExpense.amount),
+                onEditPressed: (context) => openEditBox(individualExpense, context),
+                onDeletePressed: (context) => openDeleteBox,
               );
             },
           )),
