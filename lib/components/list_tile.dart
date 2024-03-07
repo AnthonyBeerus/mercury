e32fe5ba51d11598eeedf5091d4b8ee619fb6e83
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-03-07 15:59:41 +0200

    feat: wrapped MyListTile in slidable widget

--- lib/components/list_tile.dart
+++ lib/components/list_tile.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyListTile extends StatelessWidget {
   final String title;
   final String trailing;
+  final void Function(BuildContext)? onEditPressed;
+  final void Function(BuildContext)? onDeletePressed;
 
   const MyListTile({
     super.key,
     required this.title,
     required this.trailing,
+    required this.onEditPressed,
+    required this.onDeletePressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      subtitle: Text(trailing),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          //* settings option
+          SlidableAction(
+            onPressed: onEditPressed,
+            icon: Icons.edit,
+            label: 'Edit',
+          ),
+          //* delete option
+          SlidableAction(
+            onPressed: onDeletePressed,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(trailing),
+      ),
     );
   }
 }
